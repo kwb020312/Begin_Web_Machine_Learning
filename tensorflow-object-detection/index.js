11047bdbb05c198ef5df80387c4812506e35908b
김우빈 <kwb020312@naver.com>
2023-12-13 20:49:08 +0900

    Feat: Tensorflow coco ssd object detection

--- tensorflow-object-detection/index.js
+++ tensorflow-object-detection/index.js
@@ -30,7 +30,49 @@ function enableCam(event) {
   });
 }
 
-function predictWebcam() {}
+let children = [];
 
-let model = true;
-demosSection.classList.remove("invisible");
+function predictWebcam() {
+  model.detect(video).then((predictions) => {
+    for (const curChlid of children) {
+      liveView.removeChild(curChlid);
+    }
+    children.splice(0);
+
+    for (const curPredict of predictions) {
+      if (curPredict.score > 0.66) {
+        const p = document.createElement("p");
+        p.innerText =
+          curPredict.class +
+          " - with " +
+          Math.round(parseFloat(curPredict.score) * 100) +
+          "% confidence.";
+        p.style =
+          "margin-left: " +
+          curPredict.bbox[0] +
+          "px; margin-top: " +
+          (curPredict.bbox[1] - 10) +
+          "px; width: " +
+          curPredict.bbox[2] -
+          10 +
+          "px; top: 0; left: 0;";
+        const highlighter = document.createElement("div");
+        highlighter.setAttribute("class", "highlighter");
+        highlighter.style = `left: ${curPredict.bbox[0]}px; top: ${curPredict.bbox[1]}px; width: ${curPredict.bbox[2]}px; height: ${curPredict.bbox[3]}px;`;
+
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
+
+let model = undefined;
+
+cocoSsd.load().then((loadedModel) => {
+  model = loadedModel;
+  demosSection.classList.remove("invisible");
+});
