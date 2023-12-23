7ec6674e7ff91cdd13faabcc71bf8cd92eb5253c
김우빈 <kwb020312@naver.com>
2023-12-24 00:30:11 +0900

    Feat: Load Pretrained NLP Model

--- comment-spam-detection/www/index.js
+++ comment-spam-detection/www/index.js
@@ -13,3 +13,20 @@ function handleCommentPost() {
 }
 
 POST_COMMENT_BTN.addEventListener("click", handleCommentPost);
+
+const MODEL_JSON_URL =
+  "https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/SavedModels/spam/model.json";
+const SPAM_THRESHOLD = 0.75;
+let model = undefined;
+
+async function loadAndPredict(inputTensor) {
+  if (model === undefined) model = await tf.loadLayersModel(MODEL_JSON_URL);
+
+  const results = await model.predict(inputTensor);
+
+  results.print();
+}
+
+loadAndPredict(
+  tf.tensor([[1, 3, 12, 18, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+);
